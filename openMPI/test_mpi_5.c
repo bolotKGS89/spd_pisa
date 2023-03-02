6120c6a7f893c03160cb0c018e7ba56151e162aa
Bolot <bolot.89@gmail.com>
2023-03-02 11:52:10 +0100

    round-robin done using timeout

--- openMPI/test_mpi_5.c
+++ openMPI/test_mpi_5.c
@@ -7,6 +7,7 @@
 #define TAG_RESULT 2
 #define EMITTER 0
 #define COLLECTOR 9
+#define TAG_END_STREAM 3
 
 typedef struct {
     int x;
@@ -27,10 +28,12 @@ void compute(TaskData *task, ResultData *result) {
 
 /* define your emitter process */
 void emitter(int num_workers) {
-
     MPI_Status status;
     int next_worker = 1;
     TaskData task;
+    double start_time = MPI_Wtime();
+    double timeout_seconds = 5.0; // set timeout to 10 seconds
+
      /* generate and send tasks to workers */
     srand(time(NULL)); // Seed the random number generator with the current time
     while(1) {
@@ -39,16 +42,22 @@ void emitter(int num_workers) {
         task.x = rand() % 100 + 1;
         task.y = rand() % 100 + 1;
 
-        // printf("[Process Emitter] I send value %d %d to process %d.\n", task.x, task.y, next_worker);
+        printf("[Process Emitter] I send value %d %d to process %d.\n", task.x, task.y, next_worker);
         MPI_Send(&task, sizeof(TaskData), MPI_BYTE, next_worker, TAG_JOB, MPI_COMM_WORLD);
 
-        // MPI_Recv(NULL, 0, MPI_BYTE, MPI_ANY_SOURCE, TAG_RESULT, MPI_COMM_WORLD, &status);
-        // if(status.MPI_TAG == TAG_RESULT) {
-        //     printf("done");
-        //     break;
-        // }
+                // check if timeout has expired
+        if (MPI_Wtime() - start_time > timeout_seconds) {
+            printf("Timeout expired, terminating loop.\n");
+            break;
+        }
+
+
 
         next_worker = (next_worker % num_workers) + 1;
+    }
+
+    for (int i = 1; i < num_workers; i++) {
+        MPI_Send(NULL, 0, MPI_BYTE, i, TAG_END_STREAM, MPI_COMM_WORLD);
     }
 
 }
@@ -61,9 +70,14 @@ void worker(int rank) {
 
     /* receive and process tasks from emitter */
     while (1) {
-        MPI_Recv(&task, sizeof(TaskData), MPI_BYTE, EMITTER, TAG_JOB, MPI_COMM_WORLD, &status);
+        MPI_Recv(&task, sizeof(TaskData), MPI_BYTE, EMITTER, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+        if (status.MPI_TAG == TAG_END_STREAM) {
+            MPI_Rsend(NULL, 0, MPI_BYTE, COLLECTOR, TAG_END_STREAM, MPI_COMM_WORLD);
+            printf("end workder\n");
+            break;
+        }
         compute(&task, &result);
-        // printf("[Process Worker %d] I received and computed value %.2f %.2f from process %d.\n", rank, result.x, result.y, EMITTER);
+        printf("[Process Worker %d] I received and computed value %.2f %.2f from process %d.\n", rank, result.x, result.y, EMITTER);
         MPI_Rsend(&result, sizeof(ResultData), MPI_BYTE, COLLECTOR, TAG_JOB, MPI_COMM_WORLD);
     }
 }
@@ -75,16 +89,15 @@ void collector(int num_workers) {
     ResultData result;
     MPI_Request request;
     int next_worker = 1;
-
-    // for (int i = 1; i < num_workers; i++)
-    // {
-     while(1) {    
-        MPI_Recv(&result, sizeof(ResultData), MPI_BYTE, next_worker, TAG_JOB, MPI_COMM_WORLD, &status);
+    while(1) {    
+        MPI_Recv(&result, sizeof(ResultData), MPI_BYTE, next_worker, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+        if (status.MPI_TAG == TAG_END_STREAM) {
+            printf("end collector\n");
+            break;
+        }
         printf("[Process Collecter %d] I received value %.2f %.2f from process %d.\n", COLLECTOR, result.x, result.y, next_worker);
-        next_worker = (next_worker % num_workers) + 1;
+        next_worker = (next_worker % (num_workers - 1)) + 1;
     }
-    MPI_Rsend(NULL, 0, MPI_BYTE, EMITTER, TAG_RESULT, MPI_COMM_WORLD);
-
     
 }
 
